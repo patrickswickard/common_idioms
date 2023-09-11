5a13d49b2ca07dc16b465539446e03290af8a1d2
Patrick Swickard <swickape@hotmail.com>
2023-09-11 08:47:59 -0400

    packages checkin

---
+++ packages/math.py
@@ -0,0 +1,43 @@
+import math
+
+print("math.ceil(x) returns the ceiling of x (smallest integer greater than x)")
+print("a = math.ceil(5.5)")
+a = math.ceil(5.5)
+print(a)
+
+print("\n")
+
+print("math.comb(n,k) returns the number of ways to choose k items from n items without replacement without order")
+print("a = math.comb(6,2)")
+a = math.comb(6,2)
+print(a)
+
+print("\n")
+
+print("math.copysign(x,y) returns a number with magnitude of x but sign of y, which is kinda niche")
+print("a = math.copysign(7.5,-3)")
+a = math.copysign(7.5,-3)
+print(a)
+
+print("\n")
+
+print("math.fabs(x) returns the absolute value of x")
+print("a = math.fabs(-3)")
+a = math.fabs(-3)
+print(a)
+
+print("\n")
+
+print("math.factorial(n) returns n factorial n! (integer values only)")
+print("a = math.factorial(5)")
+a = math.factorial(5)
+print(a)
+
+print("\n")
+
+print("math.floor(x) returns the floor of x (greatest integer smaller than x)")
+print("a = math.floor(5.5)")
+a = math.floor(5.5)
+print(a)
+
+print("\n")
