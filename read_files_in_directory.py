68d267f161c48281bfa78bf5de9a323e6068eb75
Patrick Swickard <swickape@hotmail.com>
2023-09-22 10:22:41 -0400

    new idiom checkin

---
+++ read_files_in_directory.py
@@ -0,0 +1,8 @@
+import re
+import os
+
+dirlist = os.listdir()
+for thisfilename in dirlist:
+  filename_base = re.search(r"(.*?)\.txt",thisfilename)
+  if filename_base:
+    print(filename_base.group(1))
