979108206f323d8523ec3d28d0f0b8ee2e777b19
Patrick Swickard <swickape@hotmail.com>
2023-12-09 09:18:42 -0500

    add list comp idiom

---
+++ list_comprehension.py
@@ -0,0 +1,16 @@
+# clunky version:
+
+fruits = ['apple', 'banana', 'cherry', 'kiwi', 'mango']
+
+newlist = []
+
+for x in fruits:
+  if 'a' in x:
+    newlist.append(x)
+
+print(newlist)
+
+# smooth version
+newlist2 = [x for x in fruits if 'a' in x]
+
+print(newlist2)
