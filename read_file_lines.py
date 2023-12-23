bc17a9be8bef4317795cfe7215ae85078216fe21
Patrick Swickard <swickape@hotmail.com>
2023-12-23 10:48:23 -0500

    pylint improvements

--- read_file_lines.py
+++ read_file_lines.py
@@ -1,6 +1,6 @@
+"""Demo read lines from file"""
 file = 'pinafore.txt'
-with open(file) as fd:
-  lines = fd.read().splitlines()
+with open(file,'r',encoding='utf-8') as myinfile:
+  lines = myinfile.read().splitlines()
   for thisline in lines:
     print(thisline)
-
